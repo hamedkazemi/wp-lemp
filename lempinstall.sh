fa85c18557df14cfd12c207e1e7a121a760a0a9a
Hamed Kazemi <hamedk@outlook.com>
2024-04-17 15:01:04 -0700

    Update lempinstall.sh

--- lempinstall.sh
+++ lempinstall.sh
@@ -22,11 +22,6 @@ ufw --force enable
 #apt-add-repository ppa:ondrej/nginx-mainline -y
 #apt-add-repository ppa:ondrej/php -y
 
-#Set up MariaDB repositories
-
-apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
-add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://mirror.netinch.com/pub/mariadb/repo/10.4/ubuntu focal main'
-
 #Install base packages
 apt-get update; apt-get install -y build-essential curl nano wget lftp unzip bzip2 arj nomarch lzop htop openssl gcc git binutils libmcrypt4 libpcre3-dev make python3 python3-pip supervisor unattended-upgrades whois zsh imagemagick uuid-runtime net-tools
 
